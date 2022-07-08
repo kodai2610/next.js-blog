abb1310bfe622d05772dcbabef7e593fb4b53f6b
koudai <koudaisports26@gmail.com>
2022-07-09 08:50:30 +0900

    first commit

---
+++ components/Layout.js
@@ -0,0 +1,51 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+
+const name = "Koudai Code";
+export const siteTitle = "Next.js blog";
+
+function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <Image
+              src="/profile.png"
+              alt="profile"
+              width={100}
+              height={100}
+              className={`${utilStyles.borderCircle} ${styles.headerHomeImage}`}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Image
+              src="/profile.png"
+              alt="profile"
+              width={100}
+              height={100}
+              className={utilStyles.borderCircle}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div>
+          <Link href="/">ホームへ戻る</Link>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Layout;
